f8d979d20ebfdfa2f0e83e1f3fa8ea6b400b7215
CholponDev <januzakova.cholpon@gmail.com>
2025-12-12 09:31:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ let now = new Date()
         console.log(now)
 
         now.getFullYear()
-        console.log(now)
+        console.log(now.getFullYear())
 
         now.getMonth()
-        console.log(now)
+        console.log(now.getMonth())
 
         now.getDate()
-        console.log(now)
+        console.log(now.getDate())
 
         now.getDay()
-        console.log(now)
+        console.log(now.getDay())
 
         now.getHours()
-        console.log(now)
+        console.log(now.getHours())
 
         now.getMinutes()
-        console.log(now)
+        console.log(now.getMinutes())
 
         now.getSeconds()
-        console.log(now)
+        console.log(now.getSeconds())
